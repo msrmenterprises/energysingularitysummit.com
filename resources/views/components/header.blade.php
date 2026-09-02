890b255a6c68ab65e2b82aafbf5950bc09005169
msrmenterprises <connect@msrmenterprises.com>
2026-09-01 22:09:14 -0500

    sdfs

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -108,7 +108,7 @@
                   </ul>
                </li>
                <li><a class="getstarted scrollto" href="register">Register Now</a></li>
-               <li><a class="getstarted scrollto" href="https://www.isuw.in/conference-agenda-and-program" target="_blank">TESS AGENDA</a></li>
+               <li><a class="getstarted scrollto" href="https://energysingularitysummit.com/conference-agenda-and-program" target="_blank">TESS AGENDA</a></li>
 			   <!--<li style="color: green;"><a href="https://isuw.virtualmnc.com/" class="getstarted scrollto ">Attend Live Event</a></li>-->
             </ul> 
             <!-- .navbar -->
