793adfd923eb903454321fef872b13d8e367b165
msrmenterprises <connect@msrmenterprises.com>
2026-09-01 22:27:41 -0500

    sdfds

--- resources/views/components/conference-agenda-and-program-2025.blade.php
+++ resources/views/components/conference-agenda-and-program-2025.blade.php
@@ -6,12 +6,14 @@
 <p></p>
 
 <div class="k_cheading">
-		<h1>TESS 2026<span class="k_greencolor"> Agenda1</span> </h1>
+		<h1>TESS 2026<span class="k_greencolor"> AGENDA</span> </h1>
 		<img src="/public/uploads/images/kborder_bottom.png" alt=""> 
 	</div>
 <!--<h4 style="color: green;margin-left: 58px;"><a href="https://events.firstviewgroup.com/AfricaRooftopSolarCongress2021#/agenda?lang=en" target="_blank">For Agenda Click here </a></h4>-->
 
-<p style="text-align:center; font-size:16px;"><a href="images/2026/TheEnergySingularitySummitTESSBrochure.pdf.pdf" target="_blank" class="getstartedd ">Download Agenda</a></p>
+<p style="text-align:center; font-size:16px; margin-bottom: 10px;">Agenda will be uploaded closer to the event date. For details, please email us at <a href="mailto:info@energysingularitysummit.com">info@energysingularitysummit.com</a>.</p>
+
+<p style="text-align:center; font-size:16px;"><span class="getstartedd" style="display:inline-block; cursor:not-allowed; opacity:0.65; pointer-events:none;">Download Agenda</span></p>
 
 <iframe src="images/2026/TheEnergySingularitySummitTESSBrochure.pdf" height="600" width="150" title="Agenda" style="width: 800px;text-align: center;"></iframe><p></p>
 </div>
