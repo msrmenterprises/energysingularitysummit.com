e893e4616dc01f0714c96f022e293eb403751d7b
msrmenterprises <connect@msrmenterprises.com>
2026-09-06 16:17:15 -0500

    df

--- resources/views/components/innovation-awards.blade.php
+++ resources/views/components/innovation-awards.blade.php
@@ -717,7 +717,7 @@ and decide upon the winners.</P>
     </h2>
     <div id="flush-collapseselection" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
       <div class="accordion-body">
-<P STYLE="margin-bottom: 0in"><IMG SRC="/images/2026/selection3.png?id=1"></P>
+<P STYLE="margin-bottom: 0in"><IMG SRC="/images/2026/selection4.png?id=1"></P>
 <P STYLE="margin-bottom: 0in"><BR>
 </P>
 </div></div></div></div>
