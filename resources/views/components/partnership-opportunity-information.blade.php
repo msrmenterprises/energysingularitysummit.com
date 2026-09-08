a9df74c40059b63645036afaab1762ed31328f1f
msrmenterprises <connect@msrmenterprises.com>
2026-09-07 21:42:58 -0500

    sdfdsf

--- resources/views/components/partnership-opportunity-information.blade.php
+++ resources/views/components/partnership-opportunity-information.blade.php
@@ -6,15 +6,15 @@
 <p></p>
 <p></p>		
 <div class="k_cheading">
-		<h1>For ISUW 2026 <span class="k_greencolor">Partnership Opportunity  </span> </h1>
+		<h1>For TESS 2027 <span class="k_greencolor">Partnership Opportunity  </span> </h1>
 		<img src="/public/uploads/images/kborder_bottom.png" alt=""> 
 	</div>
 <!--<h4 style="color: #f60;"><b>For ISUW 2022 Partnership Opportunity Information and Special Packages for Participation: </b></h4>
 <p><iframe src="http://www.isgw.in/wp-content/uploads/2020/01/For-ISUW-2020-Partnership-Opportunity-Information-and-Special-Packages-for-Participation.pdf" style="width:100%;height:600px;"></iframe><br>
 <b>Contact Team ISGF</b><br> -->
 <p><b>For Partnership Information and Special Packages for Participation<br/>
-<b>Please write to ISGF Team at <a href="mailto:isuw@isuw.in">isuw@isuw.in</a></b><br> 
-<b>Call at: +91-11-41030398<br>Please refer ISUW Brochure for details at following link: <a href="http://www.isuw.in/isuw-2025-brochure/">http://www.isuw.in/isuw-2025-brochure/</a>  </p>
+<b>Please write to ISGF Team at <a href="mailto:info@energysigularitysummit.com">info@energysigularitysummit.com</a></b><br> 
+<b>Call at: +91-11-41030398<br>Please refer TESS Brochure for details at following link: <a href="https://www.energysingularitysummit.com/isuw-brochure/">https://www.energysingularitysummit.com/isuw-brochure</a>  </p>
 <p></p>
 
 <p></p>
