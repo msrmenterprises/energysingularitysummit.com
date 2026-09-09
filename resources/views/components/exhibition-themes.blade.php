5933974af5bc02feb30cade9c8a8ae02ccd5dc73
msrmenterprises <connect@msrmenterprises.com>
2026-09-08 22:22:07 -0500

    xvc

--- resources/views/components/exhibition-themes.blade.php
+++ resources/views/components/exhibition-themes.blade.php
@@ -98,7 +98,7 @@
         </tbody>
     </table>
 </div>
-<p>To Exhibit during TESS, email us at <a href="mailto:isuw@isuw.in">isuw@isuw.in</a></p>
+<p>To Exhibit during TESS, email us at <a href="mailto:isuw@energysingularitysummit.com">isuw@energysingularitysummit.com</a></p>
 
 <p></p>
 </div>
